51c6390aab8a6a70db2f2ea795adc89172cc6813
paweldylag <pawel.dylag@estimote.com>
2017-03-28 22:47:57 +0200

    Initial commit

---
+++ src/TicTacToeGame.scala
@@ -0,0 +1,13 @@
+/**
+  *
+  * Main class for starting the game!
+  *
+  * Created by paweldylag on 28/03/2017.
+  */
+class TicTacToeGame {
+
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+
+}
